d39dda3318294c12c15e0b1fe12ecc54c7f3e17d
Harold Cindy <120691094+HaroldCindy@users.noreply.github.com>
2025-07-27 02:07:13 -0700

    Update fuzzer to work on OSX

--- fuzz.sh
+++ fuzz.sh
@@ -8,7 +8,16 @@ mkdir -p build_fuzz
 pushd build_fuzz
 mkdir -p fuzz_inputs
 
+CLANG_BIN=clang
+CLANGPP_BIN=clang++
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Make sure we're not using AppleClang because it doesn't support libfuzzer.
+  CLANG_BIN="$( brew --prefix llvm )/bin/clang"
+  CLANGPP_BIN="$( brew --prefix llvm )/bin/clang++"
+fi
+
 find ../tests/ -name "*.lsl" | grep -v "/expected/" | xargs -I'{}' cp '{}' fuzz_inputs/
-CXX=clang++ CC=clang cmake .. -DTAILSLIDE_BUILD_FUZZER=on
+CXX="${CLANGPP_BIN}" CC="${CLANG_BIN}" cmake .. -DTAILSLIDE_BUILD_FUZZER=on
 cmake --build .
 ./tailslide-fuzzer ./fuzz_inputs/ -fork=8
